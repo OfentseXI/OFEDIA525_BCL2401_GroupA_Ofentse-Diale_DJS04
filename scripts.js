f4ae37b2bd0d6e8c02e3c641d10cc9247e081029
OfentseXI <dofentse69@gmail.com>
2024-05-20 08:43:14 +0200

    final adjustments and commentary

--- scripts.js
+++ scripts.js
@@ -356,15 +356,4 @@ document
       }
     }
 
-    if (active) {
-      document.querySelector("[data-list-active]").open = true;
-      document.querySelector("[data-list-blur]").src = active.image;
-      document.querySelector("[data-list-image]").src = active.image;
-      document.querySelector("[data-list-title]").innerText = active.title;
-      document.querySelector("[data-list-subtitle]").innerText = `${
-        authors[active.author]
-      } (${new Date(active.published).getFullYear()})`;
-      document.querySelector("[data-list-description]").innerText =
-        active.description;
-    }
-  });
+    
